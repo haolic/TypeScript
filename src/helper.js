564366c37eb0b8f39c7ab90461f935c7ae888134
郝力衝 <wb-hlc336023@antfin.com>
2019-07-05 15:54:01 +0800

    dev

---
+++ src/helper.js
@@ -0,0 +1,44 @@
+import $ from 'jquery';
+
+const helper = (btnList, btnStyleList) => {
+  const getRandom = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+  const getRandomItem = (btnStyleList, num) => {
+    let str = '';
+    for (let i = 1; i < num; i++) {
+      const ran = getRandom(0, btnStyleList.length);
+      str += ` ${btnStyleList[ran]}`;
+    }
+    return str;
+  };
+  $(function() {
+    const renderDom = () => {
+      const $wrap = $('#app');
+      btnList.forEach((el, idx) => {
+        const $btn = $(
+          `<button class="button button-3d ${getRandomItem(
+            btnStyleList,
+            3
+          )} btn-custom">${el.name || 'typescript hh'}</button>`
+        ).click(() => {
+          $('#text .textarea').css({
+            height: 0
+          });
+          setTimeout(() => {
+            $('#text .textarea')
+              .css({
+                height: '80%'
+              })
+              .text(el.fn());
+          }, 300);
+        });
+        $wrap.append($btn);
+      });
+    };
+    window.rDom = document.querySelector('#text');
+    renderDom();
+  });
+};
+
+export default helper;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import './index.less';
-import $ from 'jquery';
+import helper from './helper';
 import readonly from './index.ts';
 import arrayReadonly from './arrayReadonly.ts';
 import otherPropsCheck from './otherPropsCheck.ts';
@@ -43,33 +43,4 @@ const btnStyleList = [
   'button-longshadow-left'
 ];
 
-const getRandom = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min);
-};
-const getRandomItem = (btnStyleList, num) => {
-  let str = '';
-  for (let i = 1; i < num; i++) {
-    const ran = getRandom(0, btnStyleList.length);
-    str += ` ${btnStyleList[ran]}`;
-  }
-  return str;
-};
-$(function() {
-  const renderDom = () => {
-    const $wrap = $('#app');
-    btnList.forEach((el, idx) => {
-      const $btn = $(
-        `<button class="button button-3d ${getRandomItem(
-          btnStyleList,
-          3
-        )} btn-custom">${el.name || 'typescript hh'}</button>`
-      ).click(() => {
-        $('#text').html('');
-        $('#text').html(`<textarea class="textarea" readonly>${el.fn()}</textarea>`);
-      });
-      $wrap.append($btn);
-    });
-  };
-  window.rDom = document.querySelector('#text');
-  renderDom();
-});
+helper(btnList, btnStyleList);
